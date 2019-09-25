f0fe56e1ea1b82a56c147d7751c28becc229795a
Michael Pedrotti <michael.pedrotti.11@gmail.com>
2019-09-24 22:18:06 -0300

    generated bundle

---
+++ README.md
@@ -0,0 +1,12 @@
+./client 
+
+npm install webpack-dev-server@2.5.1 --save-dev
+npm install css-loader@0.28.4  --save-dev
+npm install style-loader@0.18.2  --save-dev
+npm install url-loader@0.5.9 --save-dev
+npm install file-loader@0.11.2 --save-dev
+npm install extract-text-webpack-plugin@3.0.0 --save-dev
+npm install optimize-css-assets-webpack-plugin@2.0.0 --save-dev
+npm install cssnano@3.10.0 --save-dev
+npm install jquery@3.2.1 --save-dev
+
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,17 +1,34 @@
 const path = require("path");
 const babili = require("babili-webpack-plugin");
+const extractPlugin = require("extract-text-webpack-plugin");
+const optimizePlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
 
-let plugins = [];
+let plugins = [
+	new extractPlugin("style.css"),
+	new webpack.ProvidePlugin({
+		"$":"jquery/dist/jquery.js",
+		"jQuery":"jquery/dist/jquery.js"
+	})
+];
 
 if(process.env.NODE_ENV == 'production'){
 	plugins.push(new babili());
+	plugins.push(new optimizePlugin({
+		cssProcessor:require("cssnano"),
+		cssProcessorOptions:{
+			discardComments:{ removeAll:true }
+		},
+		canPrint:true
+	}));
 }
 
 module.exports = {
 	entry:"./app-src/app.js",
 	output:{
 		filename:"bundle.js",
-		path:path.resolve(__dirname, "dist")
+		path:path.resolve(__dirname, "dist"),
+                publicPath:'dist'
 	},
 	plugins:plugins,
 	module:{
@@ -24,7 +41,31 @@ module.exports = {
 				use:{
 					loader:'babel-loader'
 				}
-			}
+			},
+			{
+				test:/\.css$/,
+				use:extractPlugin.extract({
+					fallback:"style-loader",
+					use:"css-loader"
+				})
+				//loader:'style-loader!css-loader'
+			},
+			{ 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }  
 		]
 	}
 };
